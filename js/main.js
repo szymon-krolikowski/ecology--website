63a9f7ab139807c61d5c7cfc89ec15f01ba65218
Szymon Królikowski <krolikowski.szymon@wp.pl>
2021-03-05 09:22:18 +0100

    ADD script to mobile navigation

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ for (i = 0; i < accordion.length; i++) {
         this.classList.toggle('teamActiveBox')
     })
 }
+
+const buttonNavigationMobile = document.querySelector(".navigationMobile__button");
+const navigationMobile = document.querySelector(".navigationMobile");
+const wrapper = document.querySelector(".wrapper")
+const linksNavigationMobile = document.querySelectorAll(".navigationMobile__link")
+
+buttonNavigationMobile.addEventListener("click", () => {
+    navigationMobile.classList.toggle("navigationMobileActive");
+    buttonNavigationMobile.classList.toggle("navigationMobileButtonActive");
+});
+
+linksNavigationMobile.forEach(element => {
+    element.addEventListener("click", () => {
+        navigationMobile.classList.remove("navigationMobileActive");
+    });
+});
